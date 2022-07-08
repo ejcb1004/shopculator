998a1f027a1de0c586b0e3f1a7822c372fe3c0e3
Cerberus-0 <ayasenana036@gmail.com>
2022-07-08 16:05:21 +0800

    UserInterfaceFrontPageText

--- resources/views/shopping-lists.blade.php
+++ resources/views/shopping-lists.blade.php
@@ -1,33 +1,29 @@
 <x-app-layout>
-<<<<<<< Updated upstream:resources/views/shopping-lists.blade.php
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <x-jet-welcome />
-=======
-    <div class="py-6">
-        <div class="max-w-md mx-auto sm:px-6 lg:px-8">
-            <!-- Center Text -->
-            <div class="hero min-h-screen bg-base-200">
-                <div class="hero-content text-center">
-                    <div class="max-w-md">
-                        <h1 class="text-4xl font-bold">Make your shopping life easier with ShopCulator.</h1>
-                        <p class="py-6">With ShopCulator, an easier shopping budget management experience is at your fingertips. Save more time and money with ShopCulator.</p>
-                        <!-- Buttons -->
-                        <div class="max-w-sm space-x-10">
-                            <button class="btn btn-block transition delay-150 bg-white hover:bg-black px-6 py-2 border solid border-emerald-300 rounded-full">
-                                <div class="text-emerald-300"><i class="fa-regular fa-eye"></i> View Lists</div>
-                            </button>
-                                <button class="btn btn-block px-6 py-2 bg-emerald-300 rounded-full">
-                                    <div class="text-emerald-900"><i class="fa-solid fa-plus"></i> Create a new list</div>
-                                </button>
-                            
-                        </div>
+                <div class="py-6">
+                    <div class="max-w-md mx-auto sm:px-6 lg:px-8">
+                        <!-- Center Text -->
+                        <div class="hero min-h-screen bg-base-200">
+                            <div class="hero-content text-center">
+                                <div class="max-w-md">
+                                    <h1 class="text-4xl text-white font-bold">Make your shopping life easier with ShopCulator.</h1>
+                                    <p class="py-6 text-white">With ShopCulator, an easier shopping budget management experience is at your fingertips. Save more time and money with ShopCulator.</p>
+                                    <!-- Buttons -->
+                                    <div class="max-w-sm space-x-10">
+                                        <button class="btn btn-block transition delay-150 bg-white hover:bg-black px-6 py-2 border solid border-emerald-300 rounded-full">
+                                            <div class="text-emerald-300"><i class="fa-regular fa-eye"></i> View Lists</div>
+                                        </button>
+                                        <button class="btn btn-block px-6 py-2 bg-emerald-300 rounded-full">
+                                            <div class="text-emerald-900"><i class="fa-solid fa-plus"></i> Create a new list</div>
+                                        </button>
 
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
->>>>>>> Stashed changes:resources/views/dashboard.blade.php
-            </div>
-        </div>
-    </div>
 </x-app-layout>
