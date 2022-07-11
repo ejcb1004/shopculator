b458e6dc895364036ed2e702de8774e46c8b6244
ejcb1004 <ejc_bornales@protonmail.com>
2022-07-12 02:20:18 +0800

    Jetstream-based positional fixes

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,29 +1,36 @@
 <x-app-layout>
     <!-- Add List Nav -->
-    <div class="navbar bg-emerald-700">
-        <div class="flex-1"></div>
-        <!-- Search and Cart Button -->
-        <div>
-            <input type="text" placeholder="Search" class="input input-bordered bg-white w-96 h-8 rounded-full" />
-            <div class="dropdown dropdown-end">
-                <label tabindex="0" class="btn btn-ghost btn-circle">
-                    <div class="indicator">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="white">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                        </svg>
-                        <span class="badge badge-sm badge-secondary indicator-item">1</span>
+    <div class="bg-emerald-700 min-w-full">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="navbar flex flex-row-reverse">
+                <!-- Search and Cart Button -->
+                <div class="space-x-2">
+                    <div class="flex rounded-full bg-white w-full h-10 items-center">
+                        <i class="fa-solid fa-magnifying-glass z-99 pl-4 absolute"></i>
+                        <input type="text" placeholder="Search" class="input input-bordered bg-white pl-10 w-72 rounded-full h-10" />
                     </div>
-                </label>
-                <div tabindex="0" class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
-                    <div class="card-body">
-                        <span class="font-bold text-lg">1 Items</span>
-                        <span class="text-info">Subtotal: P69</span>
-                        <div class="card-actions">
-                            <button class="btn btn-success btn-block">Save</button>
+                    <div class="dropdown dropdown-end">
+                        <label tabindex="0" class="btn btn-ghost btn-circle">
+                            <div class="indicator">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="white">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                                </svg>
+                                <span class="badge badge-sm badge-secondary indicator-item">1</span>
+                            </div>
+                        </label>
+                        <div tabindex="0" class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
+                            <div class="card-body">
+                                <span class="font-bold text-lg">1 Items</span>
+                                <span class="text-info">Subtotal: P69</span>
+                                <div class="card-actions">
+                                    <button class="btn btn-success btn-block">Save</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 </x-app-layout>
--- resources/views/shopping-lists.blade.php
+++ resources/views/shopping-lists.blade.php
@@ -1,72 +1,80 @@
 <x-app-layout>
-    <!-- Search and Add Button -->
-    <div>
-        <div class="flex flex-row-reverse py-5 px-20">
-            <div class="flex space-x-5">
-                <input type="text" placeholder="Search" class="input input-bordered bg-white w-full max-w-xs" />
-                <button class="btn bg-emerald-700 text-white border-0 hover:bg-emerald-800 transition:1s"><i class="fa-solid fa-plus"> Add List</i></button>
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <!-- Search and Add Button -->
+        <div class="flex flex-row-reverse pt-5">
+            <div class="flex space-x-2 min-w-full justify-end">
+                <div class="flex rounded-full bg-white w-full max-w-xs h-10 items-center">
+                    <i class="fa-solid fa-magnifying-glass z-99 pl-4 absolute"></i>
+                    <input type="text" placeholder="Search" class="input input-bordered bg-white pl-10 w-full rounded-full h-10" />
+                </div>
+                <button class="bg-emerald-700 px-6 py-1.5 text-white border-none rounded-full hover:bg-emerald-800 transition:1s"><i class="fa-solid fa-plus"></i>
+                    <a href="{{ route('create') }}">
+                        &nbsp;Add List
+                    </a>
+                </button>
+            </div>
+        </div>
+        <!-- List Management Table -->
+        <div class="py-5">
+            <div class="overflow-auto rounded-lg">
+                <div data-theme="mytheme">
+                    <table class="table w-full">
+                        <!-- head -->
+                        <thead class="text-white">
+                            <tr>
+                                <th>
+                                    <label>
+                                        <input type="checkbox" class="checkbox checkbox-sm checkbox-accent" />
+                                    </label>
+                                </th>
+                                <th>LIST NAME</th>
+                                <th>TOTAL</th>
+                                <th>BUDGET</th>
+                                <th>UPDATED AT</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <!-- row 1 -->
+                            <tr class="hover">
+                                <th>
+                                    <label>
+                                        <input type="checkbox" class="checkbox checkbox-sm checkbox-accent" />
+                                    </label>
+                                </th>
+                                <td>Cy Ganderton</td>
+                                <td>Quality Control Specialist</td>
+                                <td>Blue</td>
+                                <td>00-00-00</td>
+                            </tr>
+                            <!-- row 2 -->
+                            <tr class="hover">
+                                <th>
+                                    <label>
+                                        <input type="checkbox" class="checkbox checkbox-sm checkbox-accent" />
+                                    </label>
+                                </th>
+                                <td>Hart Hagerty</td>
+                                <td>Desktop Support Technician</td>
+                                <td>Purple</td>
+                                <td>00-00-00</td>
+                            </tr>
+                            <!-- row 3 -->
+                            <tr class="hover">
+                                <th>
+                                    <label>
+                                        <input type="checkbox" class="checkbox checkbox-sm checkbox-accent" />
+                                    </label>
+                                </th>
+                                <td>Brice Swyre</td>
+                                <td>Tax Accountant</td>
+                                <td>Red</td>
+                                <td>00-00-00</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
-    <!-- List Management Table -->
-    <div class="py-5 px-20">
-        <div class="overflow-auto rounded-lg">
-            <div data-theme="mytheme">
-                <table class="table w-full">
-                    <!-- head -->
-                    <thead class="text-white">
-                        <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" class="checkbox" />
-                                </label>
-                            </th>
-                            <th>LIST NAME</th>
-                            <th>TOTAL</th>
-                            <th>BUDGET</th>
-                            <th>UPDATED AT</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        <!-- row 1 -->
-                        <tr class="hover">
-                            <th>
-                                <label>
-                                    <input type="checkbox" class="checkbox" />
-                                </label>
-                            </th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                            <td>00-00-00</td>
-                        </tr>
-                        <!-- row 2 -->
-                        <tr class="hover">
-                            <th>
-                                <label>
-                                    <input type="checkbox" class="checkbox" />
-                                </label>
-                            </th>
-                            <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
-                            <td>Purple</td>
-                            <td>00-00-00</td>
-                        </tr>
-                        <!-- row 3 -->
-                        <tr class="hover">
-                            <th>
-                                <label>
-                                    <input type="checkbox" class="checkbox" />
-                                </label>
-                            </th>
-                            <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
-                            <td>Red</td>
-                            <td>00-00-00</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
 </x-app-layout>
