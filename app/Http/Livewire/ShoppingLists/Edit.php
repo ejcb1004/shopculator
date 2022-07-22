9bcd22edd0d94c5ec8e8a59966d7ec4b67ef4404
ejcb1004 <ejc_bornales@protonmail.com>
2022-07-22 09:45:09 +0800

    Added base Livewire component

---
+++ app/Http/Livewire/ShoppingLists/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\ShoppingLists;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.shopping-lists.edit');
+    }
+}
