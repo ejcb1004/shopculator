9d5af90bf01c4affa8530bc606b4ae92be20198f
Cerberus-0 <ayasenana036@gmail.com>
2022-07-22 15:02:40 +0800

    edit components increment

--- app/Http/Livewire/ShoppingLists/Edit.php
+++ app/Http/Livewire/ShoppingLists/Edit.php
@@ -6,6 +6,31 @@ use Livewire\Component;
 
 class Edit extends Component
 {
+    public $budget;
+    public $total = 69;
+    public $items = 1;
+    public $term;
+
+
+    public $products = [
+        [
+            'image' => 'Photo',
+            'name' => 'Maling',
+            'price' => '197.00',
+        ]
+    ];
+
+
+    public function addProduct()
+    {
+        $this->products[] = [
+            'image' => 'Photo',
+            'name' => 'New Product',
+            'price' => '000.00',
+            $this->items++
+        ];
+    }
+
     public function render()
     {
         return view('livewire.shopping-lists.edit');
