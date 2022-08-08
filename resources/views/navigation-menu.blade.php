b42fd37f927ac4760c57a17e412928c5f5e85e10
ejcb1004 <ejc_bornales@protonmail.com>
2022-08-08 20:22:15 +0800

    Changed media query for main navbar

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -13,7 +13,7 @@
                 @if (Route::has('login'))
                 @auth
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 md:flex">
                     <x-jet-nav-link href="{{ route('shopping-lists') }}" :active="request()->routeIs('shopping-lists')">
                         <b>{{ __('Your Lists') }}</b>
                     </x-jet-nav-link>
@@ -24,7 +24,7 @@
 
             @if (Route::has('login'))
             @auth
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
+            <div class="hidden md:flex sm:items-center sm:ml-6">
                 <!-- Teams Dropdown -->
                 @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
                 <div class="ml-3 relative">
@@ -140,10 +140,8 @@
             @endauth
             @endif
 
-
-
             <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
+            <div class="-mr-2 md:hidden flex items-center">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -155,7 +153,7 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden md:hidden">
         <div class="pt-2 pb-3 space-y-1">
 
         </div>
