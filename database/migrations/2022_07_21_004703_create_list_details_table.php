e196c110ec941c862098b97e5adc8421b32b9fe6
ejcb1004 <ejc_bornales@protonmail.com>
2022-08-16 02:14:35 +0800

    Changed migration details

--- database/migrations/2022_07_21_004703_create_list_details_table.php
+++ database/migrations/2022_07_21_004703_create_list_details_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('list_details', function (Blueprint $table) {
             $table->id();
+            $table->integer('list_index');
             $table->string('detail_id');
             $table->string('list_id');
             $table->string('product_id');
             $table->string('image_path');
             $table->integer('quantity');
-            $table->decimal('price', 9, 2);
             $table->timestamps();
         });
     }
