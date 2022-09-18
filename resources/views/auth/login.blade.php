ac9885bbb7fb970b3019a52370a28de1486e06d6
ejcb1004 <ejc_bornales@protonmail.com>
2022-09-18 21:40:53 +0800

    Authentication and authorization complete

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,6 +15,9 @@
 
             <form method="POST" action="{{ route('login') }}">
                 @csrf
+                <div>
+                    <x-jet-input hidden id="role_id" type="text" name="role_id" value="R3" required />
+                </div>
 
                 <div>
                     <x-jet-label for="email" value="{{ __('Email') }}" />
@@ -62,7 +65,59 @@
 
             <form method="POST" action="{{ route('login') }}">
                 @csrf
+                <div>
+                    <x-jet-input hidden id="role_id" type="text" name="role_id" value="R2" required />
+                </div>
 
+                <div>
+                    <x-jet-label for="email" value="{{ __('Email') }}" />
+                    <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                </div>
+
+                <div class="mt-4">
+                    <x-jet-label for="password" value="{{ __('Password') }}" />
+                    <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                </div>
+
+                <div class="block mt-4">
+                    <label for="remember_me" class="flex items-center">
+                        <input type="checkbox" class="checkbox checkbox-accent checkbox-sm" id="remember_me" name="remember" />
+                        <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    </label>
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                    @endif
+                    &nbsp;&nbsp;
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                        {{ __('Or are you a new user?') }}
+                    </a>
+                    &nbsp;&nbsp;
+                    <button class="sc-btn-primary px-6 py-1">
+                        {{ __('Log in') }}
+                    </button>
+                </div>
+            </form>
+        </x-slot>
+
+        <x-slot name="admin">
+            <x-jet-validation-errors class="mb-4" />
+
+            @if (session('status'))
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ session('status') }}
+            </div>
+            @endif
+
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div>
+                    <x-jet-input hidden id="role_id" type="text" name="role_id" value="R1" required />
+                </div>
                 <div>
                     <x-jet-label for="email" value="{{ __('Email') }}" />
                     <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -65,7 +65,7 @@
         <x-slot name="market">
             <x-jet-validation-errors class="mb-4" />
             
-            <form method="POST" action="{{ route('register-market') }}">
+            <form method="POST" action="{{ route('register') }}">
                 @csrf
                 <div>
                     <x-jet-input hidden id="role_id" type="text" name="role_id" value="R2" required />
@@ -73,6 +73,64 @@
 
                 <div>
                     <x-jet-label for="name" value="{{ __('Market Name') }}" />
+                    <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                </div>
+
+                <div class="mt-4">
+                    <x-jet-label for="email" value="{{ __('Email') }}" />
+                    <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                </div>
+
+                <div class="mt-4">
+                    <x-jet-label for="password" value="{{ __('Password') }}" />
+                    <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                </div>
+
+                <div class="mt-4">
+                    <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                    <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                </div>
+
+                @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
+                <div class="mt-4">
+                    <x-jet-label for="terms">
+                        <div class="flex items-center">
+                            <x-jet-checkbox name="terms" id="terms" />
+
+                            <div class="ml-2">
+                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
+                                'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms of Service').'</a>',
+                                'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Privacy Policy').'</a>',
+                                ]) !!}
+                            </div>
+                        </div>
+                    </x-jet-label>
+                </div>
+                @endif
+
+                <div class="flex items-center justify-end mt-4">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                        {{ __('Already registered?') }}
+                    </a>
+                    &nbsp;&nbsp;
+                    <button class="sc-btn-primary px-6 py-1">
+                        {{ __('Register') }}
+                    </button>
+                </div>
+            </form>
+        </x-slot>
+
+        <x-slot name="admin">
+            <x-jet-validation-errors class="mb-4" />
+
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
+                <div>
+                    <x-jet-input hidden id="role_id" type="text" name="role_id" value="R3" required />
+                </div>
+
+                <div>
+                    <x-jet-label for="name" value="{{ __('Username') }}" />
                     <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
                 </div>
 
