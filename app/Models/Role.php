b9b587d1d217b9a7602121198fe908286e4d5de1
ejcb1004 <ejc_bornales@protonmail.com>
2022-09-23 15:41:17 +0800

    Model definition

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,4 +8,32 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+
+    protected $table = 'categories';
+    
+    protected $primaryKey = 'id';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'role_id',
+        'role_name'
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->hasMany(User::class, 'user_id');
+    }
 }
