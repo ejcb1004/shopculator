0c6f88ebdbc9740971cb6729dabf2dbffb947d1e
ejcb1004 <ejc_bornales@protonmail.com>
2022-10-30 04:32:39 +0800

    Added headings and mapped fields

--- app/Exports/ProductsExport.php
+++ app/Exports/ProductsExport.php
@@ -5,9 +5,10 @@ namespace App\Exports;
 use App\Models\Product;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithStrictNullComparison;
 
-class ProductsExport implements FromQuery, WithStrictNullComparison
+class ProductsExport implements FromQuery, WithStrictNullComparison, WithHeadings
 {
     use Exportable;
 
@@ -16,8 +17,36 @@ class ProductsExport implements FromQuery, WithStrictNullComparison
         $this->market_id = $market_id;
     }
 
+    public function headings(): array
+    {
+        return [
+            'product_id',
+            'market_id',
+            'subcategory_id', 
+            'product_name', 
+            'price',
+            'image_path',
+            'is_deleted'
+        ];
+    }
+
+    public function map($row): array
+    {
+        return [
+            $row->product_id,
+            $row->market_id,
+            $row->subcategory_id,
+            $row->product_name,
+            $row->price,
+            $row->image_path,
+            $row->is_deleted
+        ];
+    }
+
     public function query()
     {
-        return Product::query()->where('market_id', $this->market_id);
+        return Product::query()
+        ->select('product_id', 'market_id', 'subcategory_id', 'product_name', 'price', 'image_path', 'is_deleted')
+        ->where('market_id', $this->market_id);
     }
 }
